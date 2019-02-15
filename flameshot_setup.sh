0aebe9b263eb0a7552de004b09afe233e0c81c63
Victor Ferreira <victor.ferreira@ctvoicer.com.br>
2019-02-15 17:58:58 -0200

    pointless limitation

---
+++ flameshot_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo apt-get install -qq \
+	g++ build-essential qt5-default qt5-qmake qttools5-dev-tools \
+	libqt5dbus5 libqt5network5 libqt5core5a libqt5widgets5 libqt5gui5 libqt5svg5-dev \
+	git openssl ca-certificates
+
+git clone https://github.com/lupoDharkael/flameshot.git /tmp/flameshot
+
+cd /tmp/flameshot
+
+mkdir build
+cd build
+sudo qmake ../
+sudo make
+sudo make install
