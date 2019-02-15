bf8ebfde7312eea0bbf387f36b6320d632001ae6
Victor Ferreira <victor.ferreira@ctvoicer.com.br>
2019-02-15 17:58:27 -0200

    It worked for me...

---
+++ google_chrome_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+FILE=/tmp/google-chrome.deb
+
+sudo apt-get install -qq wget
+wget -O $FILE https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i $FILE
