f27bc58f77ef2a9b742a6869e7564c0b87d78cd7
Victor Ferreira <victor.ferreira@ctvoicer.com.br>
2019-02-15 17:59:15 -0200

    Final commit, ready for tagging

--- albert-launcher_setup-ubuntu18.04.sh
+++ albert-launcher_setup-ubuntu18.04.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install -qq curl
 curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 sudo apt-get update
-sudo apt-get install -y albert
+sudo apt-get install -qq albert
