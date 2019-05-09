b0da87ef5318245f570a1f8c4e9895b0faa421a8
Victor Otávio <victor@otavioferreira.com.br>
2019-05-09 10:40:57 -0300

    lol digg

---
+++ gnome_extensions_setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt-get install -yqq chrome-gnome-shell
+
+/usr/bin/firefox --new-tab https://extensions.gnome.org/extension/15/alternatetab/
+/usr/bin/firefox --new-tab https://extensions.gnome.org/extension/277/impatience/
