aaa504476131162a2f7666fc13e289d8f272a59c
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-05-15 11:30:03 -0300

    Committed some changes

--- albert-launcher_setup-ubuntu20.04.sh
+++ albert-launcher_setup-ubuntu20.04.sh
@@ -4,5 +4,6 @@ sudo apt-get install -yqq wget
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' > /etc/apt/sources.list.d/home:manuelschneid3r.list"
 wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_20.04/Release.key -O Release.key
 sudo apt-key add - < Release.key
-sudo apt-get update
-sudo apt-get install albert
+sudo rm -f Release.key
+sudo apt-get update -yqq
+sudo apt-get install -yqq albert
