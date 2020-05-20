2f5f67e501b378568426b494e15d9e81436731af
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2020-05-20 19:12:43 -0300

    this is Spartaaaaaaaa

---
+++ zoom_setup-ubuntu.x64.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FILE=/tmp/zoom_amd64.deb
+
+sudo apt-get install -qq wget
+wget -O $FILE https://www.zoom.us/client/latest/zoom_amd64.deb
+sudo dpkg -i $FILE
+sudo apt-get install -fyqq
