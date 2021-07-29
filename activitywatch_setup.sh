85c30d604ce6a55df17b4b631f97e521bdf41042
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2021-07-29 18:19:00 -0300

    [skip ci] I'll fix the build monday

---
+++ activitywatch_setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get install -y zip unzip
+
+LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/ActivityWatch/activitywatch/releases/latest)
+LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+ARTIFACT_URL="https://github.com/ActivityWatch/activitywatch/releases/download/$LATEST_VERSION/activitywatch-$LATEST_VERSION-linux-x86_64.zip"
+
+SETUP_FILE=/tmp/activitywatch.zip
+INSTALL_PATH=/opt/ActivityWatch
+
+if [ ! -f $SETUP_FILE ]; then
+	wget -O $SETUP_FILE $ARTIFACT_URL
+fi
+
+if [ ! -d $INSTALL_PATH ]; then
+	unzip $SETUP_FILE -C /tmp
+	sudo mv /tmp/ActivityWatch $INSTALL_PATH
+fi
