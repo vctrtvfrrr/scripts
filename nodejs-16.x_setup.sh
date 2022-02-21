b71cbf53e01bbf8a99f4090da9a3c777d7065fe5
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2022-02-21 18:05:12 -0300

    Ugh. Bad rebase.

--- nodejs-16.x_setup.sh
+++ nodejs-16.x_setup.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-NPM_GLOBAL="${HOME}/.npm-global"
-NODE_MODULES="${NPM_GLOBAL}/lib/node_modules"
-
-curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -yqq git nodejs
-
-mkdir $NPM_GLOBAL
-npm config set prefix "${NPM_GLOBAL}"
-
-mkdir -p $NODE_MODULES
-git clone https://github.com/npm/cli.git $NODE_MODULES/npm
-rm -fr $NODE_MODULES/npm/.git
-
-sudo rm -fr /usr/lib/node_modules
-sudo ln -s $NODE_MODULES /usr/lib/node_modules
-
-sudo rm -fr /usr/bin/npm
-sudo ln -s $NODE_MODULES/npm/bin/npm-cli.js /usr/bin/npm
-sudo rm -fr /usr/bin/npx
-sudo ln -s $NODE_MODULES/npm/bin/npx-cli.js /usr/bin/npx
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install 16
+nvm use 16
+nvm alias default 16
