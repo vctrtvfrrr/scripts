d34f112c99804911a709cfcd9475f979ebf8961b
Victor Otávio Ferreira <victor@otavioferreira.com.br>
2023-02-10 16:54:57 -0300

    epic

--- speedtest_steup.sh
+++ speedtest_steup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
-sudo apt install -yqq speedtest
+sudo apt-get install -yqq curl
+curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+sudo apt-get install -yqq speedtest
