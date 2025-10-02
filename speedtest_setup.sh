945019d0ba543ffbb84fef4c8e2fd41800d9f1c0
Victor Otávio Ferreira <victor.ferreira@st-one.io>
2025-10-02 16:06:23 -0300

    git stash *

--- speedtest_setup.sh
+++ speedtest_setup.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 sudo apt-get install -yqq curl
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
-sudo apt-get install -yqq speedtest
+sudo apt-get install -yqq speedtest-cli
