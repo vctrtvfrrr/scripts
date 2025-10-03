ecb39c1186af4cd1cea75162c8f6593d423fe6a5
Victor Otávio Ferreira <victor.ferreira@st-one.io>
2025-10-03 14:37:08 -0300

    Well, it's doing something.

--- stevenblack-hosts_setup.sh
+++ stevenblack-hosts_setup.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-sudo apt install -y git python3-pip
-sudo git clone https://github.com/StevenBlack/hosts.git /opt/hosts
-sudo -H pip3 install --user -r /opt/hosts/requirements.txt
-sudo -H python3 /opt/hosts/updateHostsFile.py --auto --replace --compress
+sudo apt-get update
+sudo apt-get install -y git python3 python3-pip
+sudo git clone https://github.com/StevenBlack/hosts.git ~/.config/hosts
+pip3 install --user -r ~/.config/hosts/requirements.txt
+sudo python3 ~/.config/hosts/updateHostsFile.py --auto --replace --compress
--- sublime-text_setup.sh
+++ sublime-text_setup.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo apt-get install -yqq apt-transport-https
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt-get update
-sudo apt-get install -yqq sublime-text sublime-merge
+sudo apt-get install -y sublime-text
