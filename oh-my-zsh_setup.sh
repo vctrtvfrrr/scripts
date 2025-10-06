ec54d3497112a8f3685ecdd9f506e0015d65c099
Victor Otávio Ferreira <victor@ferreira.id>
2025-10-06 16:35:00 -0300

    final commit.

--- oh-my-zsh_setup.sh
+++ oh-my-zsh_setup.sh
@@ -2,5 +2,4 @@
 set -euo pipefail
 
 sudo apt-get install -y curl git zsh
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
